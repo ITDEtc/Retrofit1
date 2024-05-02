3a214fec7a07c8f39c0f1571a974b8fbcd84a375
Erik <erikgolod9@gmail.com>
2024-05-02 13:14:09 +0900

    Retrofit1

---
+++ app/src/main/java/com/example/githubrepo/Contributor.java
@@ -0,0 +1,16 @@
+package com.example.githubrepo;
+
+class Contributor {
+
+    private String login;
+    private int id;
+    private int contributions;
+
+    // И другие поля
+    //String html_url;
+
+    @Override
+    public String toString() {
+        return "     " + login + ";" + + id + "" + contributions;
+    }}
+
